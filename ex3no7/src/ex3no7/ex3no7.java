7c2e15b8344dfca83f3efd26a78a158c8742d2b1
admin <admin@DESKTOP-8L2IG92>
2021-04-14 06:26:53 +0900

    ex3no7

---
+++ ex3no7/src/ex3no7/ex3no7.java
@@ -0,0 +1,11 @@
+package ex3no7;
+
+public class ex3no7 {   //99p
+   public static void main(String[] args) {
+		byte a = 10;
+		byte b = 20;
+        byte c = (byte)( a * b ) ;
+		System.out.println(c); 
+	   
+   }
+}
